d464aa8b6fcfca1aa4dfb983f80a416611fc26b1
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2022-01-03 16:40:11 +0100

    Added agent logic.

--- agent.py
+++ agent.py
@@ -1,77 +1,122 @@
 """Tracker Agent : Agent responsible for tracking a scan, e.g., status, data queues."""
-from time import sleep
+import multiprocessing
+import time
 import logging
-import requests
-import json
 from urllib import parse
-from typing import Dict, Optional
+from typing import Dict, Optional, List
 
-import ostorlab
+import docker
+
 from ostorlab.agent import agent
-
-
-class AuthenticationError(Exception):
-    """Authentication Error."""
+import utils
 
 
 logger = logging.getLogger(__name__)
+SLEEP_SEC = 3
+SCAN_DONE_TIMEOUT_SEC = 10
+POSTSCANE_DONE_TIMEOUT_SEC = 10
 
-SLEEP_SEC = 2
+
 class TrackerAgent(agent.Agent):
     """Agent responsible for tracking a scan."""
 
-    def _make_request(self, method: str, fullpath: str, data: Optional[Dict[str, str]] = None):
-        logger.info('Request %s %s %s', method, fullpath, data)
-        response = requests.request(method, fullpath, data=json.dumps(data))
-        if response.status_code not in [200, 201, 204]:
-            logger.error('Received %i %s', response.status_code, response.content)
-            raise AuthenticationError(response.reason)
-        elif response.status_code == 200:
-            return response.json()
-        else:
-            return None
-
-
-    #rabitmq_url='localhost:8088', user='guest', pwd='guest'
-    def list_all_queues(self, vhost='/'):
-        full_path = "http://guest:guest@localhost:15672/api/queues/"
-        #quoted_vhost = parse.quote_plus(vhost)
-        queues =  self._make_request('GET', full_path)
+    def list_all_queues(self, vhost: Optional[str] = '/') -> List[Dict]:
+        """Send a request to RabbitMQ api to list all the data queues.
+        Args:
+            vhost : String - Virtual host. Default value is '/'.
+        """
+        rabbitmq_api_path = utils.get_rabitmq_url(False)
+        quoted_vhost = parse.quote_plus(vhost)
+        queues_path = rabbitmq_api_path + f'/api/queues/{quoted_vhost}'
+        queues =  utils.make_request('GET', queues_path)
         return queues
 
-    def is_q_not_empty(self, queue):
+
+    def is_q_not_empty(self, queue: str) -> bool:
+        """Check if a data queue is not empty.
+        Agrs:
+            queue: String - Data queue name
+        Returns:
+            Boolean - False if queue is empty, else True.
+        """
         return queue['messages']  > 0 or queue['messages_unacknowledged'] > 0
 
-    def are_queues_empty(self):
+
+    def are_queues_empty(self) -> bool:
+        """Check if at least one data queue is not empty.
+        Returns:
+            Boolean - False if at least one data queue is not empty, else True.
+        """
         queues = self.list_all_queues()
         for qu in queues:
             if self.is_q_not_empty(qu):
                 return False
         return True
 
-    def periodic_checks(self):
-        sleep(SLEEP_SEC)
+    def periodic_checks(self) -> None:
+        """Periodically check the data queues, Only return of they are all empty."""
+        time.sleep(SLEEP_SEC)
+        i=0 #Just For Proof Of Concept.
         while True:
+            print('Step : ', i) # JFPOC
+            i+=1 # JFPOC
             if self.are_queues_empty():
                 return
-            sleep(SLEEP_SEC)
+            time.sleep(SLEEP_SEC)
 
+    def send_message(self, selector, message) -> None:
+        """Uses the agent.emit method."""
+        self.emit(selector, message)
 
-    def kill_universe(self):
-        print('Killing the universe.')
+    def kill_universe(self, universe_id: str) -> None:
+        """Kill all containers belonging to universe with  universe_id."""
+        print(f'Killing the universe. {universe_id}')
+        client = docker.from_env()
+        containers = client.containers.list()
+        command = 'bin/sh --login -c \'echo $UNIVERSE_ID\''
+        for container in containers:
+            cmd_output = container.exec_run(command)
+            container_universe_id = cmd_output.output.decode('utf-8')
+            print('Container : ', container, ' output : ', cmd_output)
+            if cmd_output.exit_code==0:
+                if universe_id==container_universe_id:
+                    container.kill()
+            else:
+                logger.error(cmd_output)
 
+    def checking_process_with_timeout(self, timeout: int) -> None:
+        """Method responsible for running periodic_checks inside a process with a time limit constraint.
+        Args:
+            timeout : time to wait before terminating the process.
+        """
+        check_scan_process = multiprocessing.Process(target=self.periodic_checks)
+        check_scan_process.start()
+        check_scan_process.join(timeout)
+
+        if check_scan_process.is_alive():
+            check_scan_process.kill()
+            check_scan_process.join()
 
     def start(self) -> None:
+        """Overriden method start responsible for the main logic of the agent."""
+        self.checking_process_with_timeout(SCAN_DONE_TIMEOUT_SEC)
 
-        self.periodic_checks()
+        channel = utils.get_rabbitmq_channel()
+        channel.basic_publish(exchange='', routing_key='testQ', body='Mimicking scan done emission.')
+        # 2 lines before should be changed to :
+        #self.send_message('v2.report.event.scan_agent', {'scan_id': 1, 'source': '7', 'correlation_id': '42'})
+        # after fixing the v2 protobuff msgs.
 
-        #self.emit('v2.report.event.scan_agent', {'scan_id': 1, 'source': '7', 'correlation_id': '42'})
-        self.emit('v3.healthcheck.ping', {'body': 'Hello World!'})
+        # Sleep, to wait for the message to arrive, and not get empty queues.
+        time.sleep(7)
 
-        self.periodic_checks()
+        self.checking_process_with_timeout(POSTSCANE_DONE_TIMEOUT_SEC)
 
-        self.kill_universe()
+        channel.basic_publish(exchange='', routing_key='testQ', body='Mimicking post scan done emission.')
+        #self.send_message('v2.report.event.post_scan_agent', {})
 
+        universe_id = utils.get_universe_id()
+        self.kill_universe(universe_id)
 
 
 """Next parts are only for testing purposes. will be removed later."""
@@ -82,7 +127,7 @@ agent_def = definitions.AgentDefinition(
 )
 agent_settings= definitions.AgentInstanceSettings(
     bus_url='amqp://guest:guest@localhost:55672/',
-    bus_exchange_topic='*'
+    bus_exchange_topic='/'
 )
 
 if __name__ == '__main__':
