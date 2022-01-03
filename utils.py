78bb00970c7cd383dfefcd9ad13e45e6487c51e3
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2022-01-03 16:39:55 +0100

    Added some util methods

---
+++ utils.py
@@ -0,0 +1,78 @@
+"""Utils for the tracker agent.
+Provides helping constants & methods like rabbitMQ credentials, URL,
+and sending requests for the rabbitMQ management API.
+"""
+import os
+import requests
+import json
+import logging
+from typing import Dict, Optional
+import pika  #To be removed later
+
+
+logger = logging.getLogger(__name__)
+
+USERNAME = 'guest'
+PASSWORD = 'guest'
+HOST = 'localhost'
+PORT = '15672'
+
+class AuthenticationError(Exception):
+    """Authentication Error."""
+
+def make_request(method: str, path: str, data: Optional[Dict[str, str]] = None):
+    """Method that sends an HTTP request.
+    Args:
+        method : Required - One of HTTP requests, e.g., GET, POST.
+        path : Required - Where to send the request to.
+        data : Optional - Dictionary of data to be sent in the request.
+    Returns:
+        response : in json format if status code is 200
+        None : if status code is 201 or 204
+    Raises:
+        AuthenticationError: if status code is not successful (200, 201, 204)
+    """
+    logger.info('Request %s %s %s', method, path, data)
+    response = requests.request(method, path, data=json.dumps(data))
+    if response.status_code not in [200, 201, 204]:
+        logger.error('Received %i %s', response.status_code, response.content)
+        raise AuthenticationError(response.reason)
+    elif response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+
+def get_rabitmq_url(use_https: bool) -> str:
+    """Return URL to RabbitMQ management API.
+    Args:
+        use_https: Boolean : Usage of HTTPS
+    """
+    if use_https:
+        return f'https://{USERNAME}:{PASSWORD}@{HOST}:{PORT}'
+    return f'http://{USERNAME}:{PASSWORD}@{HOST}:{PORT}'
+
+def get_universe_id() -> str:
+    """Method that returns the universe_id, stored as an environment variable.
+    Returns:
+        The universe id.
+    Raises:
+        KeyError : In case the environment variable(universe_id) does not exist.
+    """
+    try:
+        return os.environ['UNIVERSE_ID']
+    except KeyError:
+        logger.error('The %s variable does not exist in the current environment.', )
+
+#This method will be removed later
+def get_rabbitmq_channel():
+    credentials = pika.PlainCredentials('guest', 'guest')
+    parameters = pika.ConnectionParameters('localhost',
+                                    55672,
+                                    '/',
+                                    credentials)
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+
+    channel.queue_declare(queue='testQ', durable=True)
+    return channel
