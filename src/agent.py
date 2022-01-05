122b6aae6d1b17423940c5e7d3a1519f7255e3af
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2022-01-05 10:59:56 +0100

    Fixed some linting

--- src/agent.py
+++ src/agent.py
@@ -3,7 +3,7 @@ import multiprocessing
 import time
 import logging
 from urllib import parse
-from typing import Dict, Optional, List
+from typing import Any, Dict, Optional, List
 
 import docker
 
@@ -32,7 +32,7 @@ class TrackerAgent(agent.Agent):
         return queues
 
 
-    def is_q_not_empty(self, queue: str) -> bool:
+    def _is_q_not_empty(self, queue: str) -> bool:
         """Check if a data queue is not empty.
         Agrs:
             queue: String - Data queue name
@@ -42,94 +42,77 @@ class TrackerAgent(agent.Agent):
         return queue['messages']  > 0 or queue['messages_unacknowledged'] > 0
 
 
-    def are_queues_empty(self) -> bool:
+    def _are_queues_empty(self) -> bool:
         """Check if at least one data queue is not empty.
         Returns:
             Boolean - False if at least one data queue is not empty, else True.
         """
         queues = self.list_all_queues()
         for qu in queues:
-            if self.is_q_not_empty(qu):
+            if self._is_q_not_empty(qu):
                 return False
         return True
 
-    def periodic_checks(self) -> None:
-        """Periodically check the data queues, Only return of they are all empty."""
-        time.sleep(SLEEP_SEC)
-        i=0 #Just For Proof Of Concept.
+
+    def _periodic_checks(self) -> None:
+        """Periodically check the data queues, Only return if they are all empty."""
         while True:
-            print('Step : ', i) # JFPOC
-            i+=1 # JFPOC
-            if self.are_queues_empty():
+            if self._are_queues_empty():
                 return
             time.sleep(SLEEP_SEC)
 
-    def send_message(self, selector, message) -> None:
+
+    def send_message(self, selector: str, data: Dict[str, Any]) -> None:
         """Uses the agent.emit method."""
-        self.emit(selector, message)
+        self.emit(selector, data)
 
-    def kill_universe(self, universe_id: str) -> None:
-        """Kill all containers belonging to universe with  universe_id."""
-        print(f'Killing the universe. {universe_id}')
-        client = docker.from_env()
-        containers = client.containers.list()
-        command = 'bin/sh --login -c \'echo $UNIVERSE_ID\''
-        for container in containers:
-            cmd_output = container.exec_run(command)
-            container_universe_id = cmd_output.output.decode('utf-8')
-            print('Container : ', container, ' output : ', cmd_output)
-            if cmd_output.exit_code==0:
-                if universe_id==container_universe_id:
-                    container.kill()
-            else:
-                logger.error(cmd_output)
 
-    def checking_process_with_timeout(self, timeout: int) -> None:
+    def checking_process_with_timeout(self, timeout: int, selector: str) -> None:
         """Method responsible for running periodic_checks inside a process with a time limit constraint.
+        Kills the process and emits a message in case it exceeded the time limit.
         Args:
             timeout : time to wait before terminating the process.
         """
-        check_scan_process = multiprocessing.Process(target=self.periodic_checks)
+        check_scan_process = multiprocessing.Process(target=self._periodic_checks)
         check_scan_process.start()
         check_scan_process.join(timeout)
 
         if check_scan_process.is_alive():
             check_scan_process.kill()
             check_scan_process.join()
+            self.send_message(selector, {})
+
+
+    def kill_universe(self, universe_id: str) -> None:
+        """remove a service belonging to universe with universe_id."""
+        client = docker.from_env()
+        services = client.services.list()
+        for s in services:
+            try:
+                service_env_variables = s.attrs['Spec']['TaskTemplate']['ContainerSpec']['Env']
+                for variable in service_env_variables:
+                    if 'UNIVERSE_ID' in variable and variable.split('=')[-1]==universe_id:
+                        s.remove()
+            except KeyError:
+                logger.error('The environement variables do not exist.')
+
 
     def start(self) -> None:
         """Overriden method start responsible for the main logic of the agent."""
-        self.checking_process_with_timeout(SCAN_DONE_TIMEOUT_SEC)
 
-        channel = utils.get_rabbitmq_channel()
-        channel.basic_publish(exchange='', routing_key='testQ', body='Mimicking scan done emission.')
-        # 2 lines before should be changed to :
-        #self.send_message('v2.report.event.scan_agent', {'scan_id': 1, 'source': '7', 'correlation_id': '42'})
-        # after fixing the v2 protobuff msgs.
+        self.checking_process_with_timeout(
+            SCAN_DONE_TIMEOUT_SEC,
+            'v3.report.event.scan.timeout'
+            )
 
-        # Sleep, to wait for the message to arrive, and not get empty queues.
-        time.sleep(7)
+        self.send_message('v3.report.event.scan.done', {})
 
-        self.checking_process_with_timeout(POSTSCANE_DONE_TIMEOUT_SEC)
+        self.checking_process_with_timeout(
+            POSTSCANE_DONE_TIMEOUT_SEC,
+            'v3.report.event.post_scan.timeout'
+        )
 
-        channel.basic_publish(exchange='', routing_key='testQ', body='Mimicking post scan done emission.')
-        #self.send_message('v2.report.event.post_scan_agent', {})
+        self.send_message('v3.report.event.post_scan.done', {})
 
         universe_id = utils.get_universe_id()
         self.kill_universe(universe_id)
-
-
-"""Next parts are only for testing purposes. will be removed later."""
-from ostorlab.runtimes import definitions
-agent_def = definitions.AgentDefinition(
-    name='AgentName',
-    out_selectors=['v3.healthcheck.ping']  #, 'v2.report.event.scan_agent']
-)
-agent_settings= definitions.AgentInstanceSettings(
-    bus_url='amqp://guest:guest@localhost:55672/',
-    bus_exchange_topic='/'
-)
-
-if __name__ == '__main__':
-    trackerAgent = TrackerAgent(agent_def, agent_settings)
-    trackerAgent.run()
