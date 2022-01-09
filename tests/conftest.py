a5e00bf2fd0765bb651657783bfa8910ba2673c6
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2022-01-09 12:16:22 +0100

    updated agent construction

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,6 +12,10 @@ import src.agent as agent_tracker
 from ostorlab.agent import definitions as agent_definitions
 from ostorlab.runtimes import definitions as runtime_definitions
 
+SCAN_DONE_TIMEOUT_SEC = 0.2
+POSTSCANE_DONE_TIMEOUT_SEC = 0.2
+
+
 @pytest.fixture(scope='function', name='tracker_agent')
 def fixture_tracker_agent():
     """Instantiate a tracker agent."""
@@ -26,7 +30,10 @@ def fixture_tracker_agent():
     settings = runtime_definitions.AgentSettings(
         key='agent_tracker_key',
         bus_url='NA',
-        bus_exchange_topic='NA')
+        bus_exchange_topic='NA',
+        bus_managment_url='http://guest:guest@localhost:15672',
+        bus_vhost='/',
+        )
 
-    agent = agent_tracker.TrackerAgent(definition, settings)
+    agent = agent_tracker.TrackerAgent(definition, settings, SCAN_DONE_TIMEOUT_SEC, POSTSCANE_DONE_TIMEOUT_SEC)
     return agent
