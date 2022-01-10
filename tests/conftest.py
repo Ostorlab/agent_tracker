b1f807d98b5caa445d85f0bc7ef6b98d189fdd67
Abderrahim HADDADI <abderrahim.haddadi@ostorlab.dev>
2022-01-10 09:30:47 +0100

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 """Pytest fixture for the tracker agent."""
 import pytest
 
-import agent as agent_tracker
+from src import agent as agent_tracker
 from ostorlab.agent import definitions as agent_definitions
 from ostorlab.runtimes import definitions as runtime_definitions
 
