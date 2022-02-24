97fbc04b7d51e1289c8fc0f7e731b4203a03dcca
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2022-02-24 18:25:12 +0100

    Universe logs.

--- agent/universe.py
+++ agent/universe.py
@@ -3,6 +3,7 @@ import logging
 
 import docker
 
+
 logger = logging.getLogger(__name__)
 
 
@@ -10,20 +11,20 @@ def kill_universe(universe_id: str) -> None:
     """remove a service belonging to universe with universe_id."""
     client = docker.from_env()
     services = client.services.list()
-    logger.info('Stopping the scan services')
+    logger.info('stopping the scan services')
     for s in services:
         service_env_variables = s.attrs['Spec'].get('Labels') or {}
         if service_env_variables.get('ostorlab.universe') == universe_id:
             s.remove()
 
-    logger.info('Stopping the scan network')
+    logger.info('stopping the scan network')
     networks = client.networks.list()
     for network in networks:
         network_labels = network.attrs.get('Labels') or {}
         if network_labels.get('ostorlab.universe') == universe_id:
             network.remove()
 
-    logger.info('Stopping the scan configs')
+    logger.info('stopping the scan configs')
     configs = client.configs.list()
     for config in configs:
         config_labels = config.attrs['Spec'].get('Labels') or {}
